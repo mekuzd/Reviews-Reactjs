15a8f74ef312d83a4672a272faa02758da2aae8f
Mekuzd <emekaseun.es@gmail.com>
2022-12-09 01:32:31 +0100

    final commit

--- src/Review.js
+++ src/Review.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
   const checkNumber = (number) => {
     if (number > people.length - 1) {
       return 0;
-    }
-    if (number < 0) {
+    } else if (number < 0) {
       return people.length - 1;
     }
     return number;
   };
   const nextPerson = () => {
     setIndex((index) => {
-      let newIndex = index + 1;
+      let newIndex = index;
       return checkNumber(newIndex);
     });
   };
@@ -34,25 +33,25 @@ const Review = () => {
   };
 
   return (
-    <article className='review'>
-      <div className='img-container'>
-        <img src={image} alt={name} className='person-img' />
-        <span className='quote-icon'>
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
           <FaQuoteRight />
         </span>
       </div>
-      <h4 className='author'>{name}</h4>
-      <p className='job'>{job}</p>
-      <p className='info'>{text}</p>
-      <div className='button-container'>
-        <button className='prev-btn' onClick={prevPerson}>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className='next-btn' onClick={nextPerson}>
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn' onClick={randomPerson}>
+      <button className="random-btn" onClick={randomPerson}>
         surprise me
       </button>
     </article>
